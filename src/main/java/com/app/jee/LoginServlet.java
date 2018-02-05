7b42cffcec551eef7d19ecaeea4bbe5d8de799d1
Ameya Deshmukh <ameya03deshmukh@gmail.com>
2018-02-04 19:59:05 -0500

    Spring MVC setup

--- src/main/java/com/app/jee/LoginServlet.java
+++ src/main/java/com/app/jee/LoginServlet.java
@@ -1,4 +1,4 @@
-package web_applications;
+package com.app.jee;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
--- src/main/java/com/app/jee/UserValidationService.java
+++ src/main/java/com/app/jee/UserValidationService.java
@@ -1,4 +1,4 @@
-package web_applications;
+package com.app.jee;
 
 public class UserValidationService {
 
